019026c3c3e450a5481e0fec6adbf0e6233fff4f
cheneyweb <457299596@qq.com>
2017-03-25 21:43:54 +0800

    代码与文档整理

--- README.md
+++ README.md
@@ -5,44 +5,48 @@
 >
 	1,config/default.js中设置数据库连接，执行npm install
 	2,node app.js(启动)
-	3,启动app.js后，系统会自动加载model/下所有的JS文件，并且与数据库同步，创建数据库表或更新字段
+
+注意事项
+>
+	1,启动app.js后，系统会自动加载/src/model/下所有的JS文件，并且与数据库同步，创建数据库表或更新字段
+	2,切换到生产环境需要执行命令 export NODE_ENV=production
 
 命名规则
 >
-	Model文件名需要以首字母大写的驼峰法命名，例如应该是UserInfo.js，而不是userInfo.hs或user_info.js
+	Model文件名需要以【首字母大写】的【驼峰法】命名，例如应该是UserInfo.js，而不是userInfo.hs或user_info.js
 	RESTful请求url，model名称【下划线分割】
 
 框架目录结构（后台）
 >
     xmodel
+    	src/app.js——系统入口
+    	src/config——系统环境配置模块
+        src/model——【数据模型实体，这是使用框架唯一需要编写的文件，每增加数据实体，增加一个文件】
         xmodel_modules/controller——请求控制器，全自动，无须人工处理
         xmodel_modules/router——路由器控制器，全自动，无须人工干预
         xmodel_modules/sequelize——ORM数据库映射器，全自动，无须人工干预
         xmodel_modules/auth——passport认证模块
-        src/config——系统环境配置模块
-        src/model——【数据模型实体，这是使用框架唯一需要编写的文件，每增加数据实体，增加一个文件】
-        src/app.js——系统入口
 
 RESTful规则
 >
-	[POST]http://host:port/model/create
-	[POST]http://host:port/model/update
-	[POST]http://host:port/model/query
-	[GET]http://host:port/model/get/:id
-	[GET]http://host:port/model/destroy/:id
+	[POST]http://host:port/xmodel/MODEL/create
+	[POST]http://host:port/xmodel/MODEL/update
+	[POST]http://host:port/xmodel/MODEL/query
+	[GET]http://host:port/xmodel/MODEL/get/:id
+	[GET]http://host:port/xmodel/MODEL/destroy/:id
 
 例子
 >
 	以一个用户模块为例，需要对用户进行增删改查:
 	需要注意的是默认自动创建id,createdAt,updatedAt三个字段，无须人工处理
-	[POST]http://host:port/user_model/create
+	[POST]http://host:port/xmodel/user_model/create
 		post body:{"username":"cheney","password":"123"}
-	[POST]http://host:port/user_model/update
+	[POST]http://host:port/xmodel/user_model/update
 		post body:{id:1,"username":"cheney","password":"456"}
-	[POST]http://host:port/user_model/query
+	[POST]http://host:port/xmodel/user_model/query
 		post body:{"username":"cheney","password":"123"}
-	[GET]http://host:port/user_model/get/1
-	[GET]http://host:port/user_model/destroy/1
+	[GET]http://host:port/xmodel/user_model/get/1
+	[GET]http://host:port/xmodel/user_model/destroy/1
 
 框架整合（开源力量）
 >
@@ -69,3 +73,4 @@ RESTful规则
 	2017.03.11:无后端理念确认，1.0版本推出
 	2017.03.12:增加系统环境配置，增加日志模块，增加权限认证模块
 	2017.03.18:chenxingling优化项目结构
+	2017.03.25:整理代码与文档
--- xmodel_modules/router/router.js
+++ xmodel_modules/router/router.js
@@ -1,44 +1,46 @@
+// 路由相关
 var express = require('express');
 var router = express.Router();
+// 认证与日志
 var passport = require(__dirname + '/../auth/passport_config.js');
 var log = require('tracer').colorConsole({ level: require('config').get('log').level });
-
+// 路径信息
 const CONTROLLER_PATH = __dirname + '/../controller/ModelController.js';
 const MODEL_SUFFIX = '.js';
 
 // 配置路由与Controller方法的绑定
 // 创建实体对象
-router.post('/*/create', function(req, res) {
+router.post('/xmodel/*/create', function(req, res) {
     // 从请求路径中获取Controller名称
-    req.modelName = transJavaStyle(ucfirst(req.path.split('/')[1])) + MODEL_SUFFIX;
+    req.modelName = transJavaStyle(ucfirst(req.path.split('/')[2])) + MODEL_SUFFIX;
     // 动态加载对应名称的方法
     require(CONTROLLER_PATH).create(req, res);
 });
 // 更新实体对象
-router.post('/*/update', function(req, res) {
+router.post('/xmodel/*/update', function(req, res) {
     // 从请求路径中获取Controller名称
-    req.modelName = transJavaStyle(ucfirst(req.path.split('/')[1])) + MODEL_SUFFIX;
+    req.modelName = transJavaStyle(ucfirst(req.path.split('/')[2])) + MODEL_SUFFIX;
     // 动态加载对应名称的方法
     require(CONTROLLER_PATH).update(req, res);
 });
 // 复杂查询实体对象
-router.post('/*/query', function(req, res) {
+router.post('/xmodel/*/query', function(req, res) {
     // 从请求路径中获取Controller名称
-    req.modelName = transJavaStyle(ucfirst(req.path.split('/')[1])) + MODEL_SUFFIX;
+    req.modelName = transJavaStyle(ucfirst(req.path.split('/')[2])) + MODEL_SUFFIX;
     // 动态加载对应名称的方法
     require(CONTROLLER_PATH).query(req, res);
 });
 // 销毁实体对象(删除时需要登录认证权限)
-router.get('/*/destroy/:id', function(req, res) {
+router.get('/xmodel/*/destroy/:id', function(req, res) {
     // 从请求路径中获取Controller名称
-    req.modelName = transJavaStyle(ucfirst(req.path.split('/')[1])) + MODEL_SUFFIX;
+    req.modelName = transJavaStyle(ucfirst(req.path.split('/')[2])) + MODEL_SUFFIX;
     // 动态加载对应名称的方法
     require(CONTROLLER_PATH).destroy(req, res);
 });
 // 获取实体对象
-router.get('/*/get/:id', function(req, res) {
+router.get('/xmodel/*/get/:id', function(req, res) {
     // 从请求路径中获取Controller名称
-    req.modelName = transJavaStyle(ucfirst(req.path.split('/')[1])) + MODEL_SUFFIX;
+    req.modelName = transJavaStyle(ucfirst(req.path.split('/')[2])) + MODEL_SUFFIX;
     // 动态加载对应名称的方法
     require(CONTROLLER_PATH).get(req, res);
 });
