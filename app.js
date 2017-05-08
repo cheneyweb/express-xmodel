6d241fe3b0b252bbd18df52cd7ba8aafb6f38a57
cheneyweb <457299596@qq.com>
2017-05-08 22:33:07 +0800

    express-xmodel 1.0

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const controllerRoot = config.get('server').controllerRoot
 const express = require('express')
 const bodyParser = require('body-parser')
 const xmodel = require(__dirname + '/xmodel_modules/express-xmodel/index.js')
-// 认证相关
-const expressSession = require('express-session')
-const flash = require('connect-flash')
-const passport = require(__dirname + '/src/auth/passport_config.js')
 // 持久层相关
 const fs = require('fs')
 const sequelize = require(__dirname + '/src/sequelize/sequelize.js')
@@ -21,23 +17,6 @@ const log = require('tracer').colorConsole({ level: config.get('log').level })
 // 初始化应用服务器
 const app = express()
 app.use(bodyParser.json())
-app.use(expressSession({
-    secret: 'cheneyxu',
-    resave: false,
-    saveUninitialized: false
-}))
-// 初始化调用 passport
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(flash())
-app.use('/',function(req,res,next){
-	log.info('统一权限控制')
-	if(true){
-		next()
-	}else{
-		res.send('权限校验失败')
-	}
-})
 
 /**
  * 使用路由统一控制(目前支持以下5种RESTful请求)
